b393cd4a109cb20e313cc8c35bc2842d2fd529a9
Renas Brko <renas.mh4925@stu.uod.ac>
2025-12-31 23:41:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function toggleDark() {
-  document.body.classList.toggle("dark");
-}
+const langBtn = document.getElementById("langToggle");
+const darkBtn = document.getElementById("toggleDark");
+
+let currentLang = "ar";
+
+/* Dark Mode */
+darkBtn?.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+});
+
+/* Language Toggle */
+langBtn.addEventListener("click", () => {
+  currentLang = currentLang === "ar" ? "en" : "ar";
+
+  document.documentElement.lang = currentLang;
+  document.documentElement.dir = currentLang === "ar" ? "rtl" : "ltr";
+
+  langBtn.textContent = currentLang === "ar" ? "EN" : "AR";
+
+  [dir="ltr"] .text-right { text-align: left; }
+  [dir="rtl"] .text-left { text-align: right; }
+
+
+  document.querySelectorAll("[data-ar]").forEach(el => {
+    el.textContent = el.getAttribute(`data-${currentLang}`);
+  });
+});
